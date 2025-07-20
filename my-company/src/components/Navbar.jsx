4d1aa6fdeaec7f0681396cb92b82d61286bc9e08
RudoCoder <rudomakuwe@gmail.com>
2025-07-20 21:46:51 +0200

    Updated file

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,14 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ background: '#333', padding: '10px' }}>
-      <Link to="/" style={{ color: '#fff', margin: '0 15px' }}>Home</Link>
-      <Link to="/about" style={{ color: '#fff', margin: '0 15px' }}>About</Link>
-      <Link to="/services" style={{ color: '#fff', margin: '0 15px' }}>Services</Link>
-      <Link to="/contact" style={{ color: '#fff', margin: '0 15px' }}>Contact</Link>
+    <nav
+      style={{
+        backgroundColor: '#333',
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: '10px'
+      }}
+    >
+      <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
