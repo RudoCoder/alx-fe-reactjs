c8c359aaaf3e2528c07c8a6aa974d0698f8b4c4e
RudoCoder <rudomakuwe@gmail.com>
2025-07-27 22:20:33 +0200

    Update Recipe List

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useRecipeStore from '../store/recipeStore';
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const filteredRecipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
