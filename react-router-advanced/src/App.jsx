5f5768e2b95bfc7b81608e50f58aca898e35f570
RudoCoder <rudomakuwe@gmail.com>
2025-08-24 13:42:41 +0200

    changes to th file path

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import BlogPost from "./pages/BlogPost";
-import Login from "./pages/Login";
+import Home from "./components/Home";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
+import Login from "./components/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
